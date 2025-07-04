e44269d3d76d68d47c6e2e64d2f2e7f87710c96e
Vichugoramo <vichugoramo@gmail.com>
2025-07-03 23:08:50 -0600

    Primer JS, con pequenas pruebas de funciones

---
+++ funciones.js
@@ -0,0 +1,17 @@
+function saludar(){
+    console.log("Hola amigos y suscriptores de youtube");
+}
+
+saludar();
+
+function sumar(num1, num2){
+    const resultado = num1 + num2;
+    return resultado
+}
+
+const suma = sumar(5,2)
+
+console.log("el resultado es: "+suma)
+
+const suma2 = sumar(10,10)
+console.log("Sexo2: "+suma2)
